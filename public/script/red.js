ec395d35a310d44510e1d8bba2e477475578559e
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-04-19 11:53:44 +0200

    server-side posting

--- public/script/red.js
+++ public/script/red.js
@@ -1,7 +1,14 @@
+// sidebar
 const menuBtn = document.querySelector('.menuBtn');
 const menu_exit = document.querySelector(".menu_exit");
 const sidebar = document.getElementById("sidebar");
 const appear = document.querySelector(".appear");
+
+// forms
+let forms = document.querySelectorAll('form');
+const formBTN = document.querySelectorAll('button[type="submit"]');
+const testBTN = document.querySelector(".inleiding_text");
+
 
 menuBtn.addEventListener('click' ,function(e){
 	sidebar.classList.add(appear)
@@ -10,8 +17,42 @@ menuBtn.addEventListener('click' ,function(e){
 });
 
 menu_exit.addEventListener('click',(e)=>{
-	sidebar.classList.remove(appear)
-	console.log('went out')
-	console.log(e)
+	sidebar.classList.remove('appear')
+	console.log('went out');
+	console.log(e);
 });
 
+if(formBTN){
+	forms.forEach(form =>{
+		form.addEventListener('submit', function(e){
+	
+
+			let data = new FormData(this)
+			// console.log(data,"and",this)
+			data.append('enhanced', true)
+			
+
+			fetch(this.action,{
+				body: new URLSearchParams(data),
+				method: this.method,
+				
+			})
+			.then(function(response){
+				return response.text()
+				// ,
+				console.log('[3]',response.text());
+			}).catch(error => {
+                console.error('Error occurred:', error);
+            });
+
+			e.preventDefault()
+
+
+		})
+	})
+	testBTN.addEventListener('click',()=>{
+		console.log(forms);
+
+	})
+
+}
